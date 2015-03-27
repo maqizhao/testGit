8eb572223c433fd1581b22116bc49647e94f31ff
maqizhao <maqizhao@126.com>
2015-03-27 16:11:56 +0800

    Initial Commit

---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by cqvip on 15/3/27.
+//  Copyright (c) 2015年 cqvip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testGit
+//
+//  Created by cqvip on 15/3/27.
+//  Copyright (c) 2015年 cqvip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
